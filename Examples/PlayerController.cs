ffc274514476113d04f4619561fbaefd6e7a059a
CoffeeVampir3 <48565901+CoffeeVampir3@users.noreply.github.com>
2020-09-10 08:05:27 +0200

    Update PlayerController.cs

--- Examples/PlayerController.cs
+++ Examples/PlayerController.cs
@@ -16,14 +16,14 @@ namespace BehaviourGraph.Debugging
         public float speed = 1f;
         public void Update()
         {
-            var whores = Input.GetAxis("Horizontal");
+            var horz = Input.GetAxis("Horizontal");
             var verts = Input.GetAxis("Vertical");
 
             var transform1 = transform;
             
             var p = transform1.localPosition;
-            p += new Vector3(whores * speed, verts * speed);
-
+            p += new Vector3(horz * speed, verts * speed);
+            
             transform1.localPosition = p;
         }
     }
