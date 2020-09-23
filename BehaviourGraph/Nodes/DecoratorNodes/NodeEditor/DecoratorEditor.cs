725f4b3901f76b22092edb54148d5ee4e1eb254a
CoffeeVampir3 <48565901+CoffeeVampir3@users.noreply.github.com>
2020-09-23 07:27:20 +0200

    Added Node Editors

---
+++ BehaviourGraph/Nodes/DecoratorNodes/NodeEditor/DecoratorEditor.cs
@@ -0,0 +1,16 @@
+﻿using Coffee.Behaviour.Nodes.DecoratorNodes;
+using UnityEngine;
+using XNodeEditor;
+
+namespace Coffee.Behaviour.Nodes.LeafNodes
+{
+    [NodeEditor.CustomNodeEditorAttribute(typeof(DecoratorNode))]
+    public class DecoratorEditor : NodeEditor
+    {
+        private static Color GuiColor = new Color(76/255f, 92/255f, 104/255f, 220/255f);
+        public override Color GetTint()
+        {
+            return GuiColor;
+        }
+    }
+}
